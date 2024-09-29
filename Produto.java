fa65d3171ae80e5b758921da0b3d2ec345c85ad1
Letícia Rosa <leticiasxrosa@gmail.com>
2024-09-29 11:10:06 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,18 @@
+package Venda;
+
+public class Produto {
+    
+    public int id;
+    public String nome;
+    public Double prec;
+
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public Double getPrec() { return prec; }
+
+    public void setId(int id) { this.id = id; }
+    public void setNome(String nome) { this.nome = nome; }
+    public void setPrec(Double prec) { this.prec = prec; }
+
+    
+}
