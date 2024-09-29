8a5520bc5068b54d820cebee6ce7f2e573cb4171
Letícia Rosa <leticiasxrosa@gmail.com>
2024-09-29 11:49:48 -0300

    Update Realizacao_venda.java
    Código incompleto, a ser atualizado

--- Venda/Realizacao_venda.java
+++ Venda/Realizacao_venda.java
@@ -1,1 +1,33 @@
+package Venda;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Realizacao_venda {
+
+    Scanner ler = new Scanner(System.in);
+    float pago, total, troco;
+    Produto p = new Produto();
+    List<Realizacao_venda> vendaAtual = new ArrayList<Realizacao_venda>();
+
+    void vender(){
+        System.out.printf("ID do produto: "); //ID do produto vai ser seu índice na lista
+        p.id = ler.nextInt();
+        System.out.printf("Nome do produto: ");
+        p.nome = ler.nextLine();
+        System.out.printf("Preço: ");
+        p.prec = ler.nextDouble();
+        System.out.println("ID: "+p.getId()+"\nNome: "+p.getNome()+"\nDescricao: "+p.getPrec());
+
+        produto.add(vendaAtual);
+
+
+        ler.close();
+    }
+  
+    public static void main(String[] args) {
+        
+    }
+
+}
